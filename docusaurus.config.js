9b24208bea2c05969e5a6b66aee3514d0b66f23a
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-05-11 18:26:00 +0800

    chore: update branch name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: "https://github.com/nation3/wiki/tree/master/",
+          editUrl: "https://github.com/nation3/wiki/tree/main/",
           routeBasePath: "/",
         },
         blog: false,
